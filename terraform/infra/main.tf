9a7c9cd07596af7f226ed8203e6929c1412c5b7c
Matheus Pimenta <matheuscscp@gmail.com>
2025-05-30 13:36:26 +0100

    Add ACR artifact registry
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -9,6 +9,11 @@ terraform {
       source  = "hashicorp/google"
       version = "6.37.0"
     }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.31.0"
+    }
   }
 }
 
@@ -24,6 +29,35 @@ provider "google" {
 locals {
   gcp_project        = "flux-gitops-playground"
   gcp_project_number = "234382750829"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id      = local.azure_subscription_id
+  use_cli              = false
+  use_oidc             = true
+  client_id_file_path  = var.tfc_azure_dynamic_credentials.default.client_id_file_path
+  oidc_token_file_path = var.tfc_azure_dynamic_credentials.default.oidc_token_file_path
+  tenant_id            = "4bd9a3a0-4393-4c05-ab26-77f6c0bfc693"
+}
+
+variable "tfc_azure_dynamic_credentials" {
+  description = "Object containing Azure dynamic credentials configuration"
+  type = object({
+    default = object({
+      client_id_file_path  = string
+      oidc_token_file_path = string
+    })
+    aliases = map(object({
+      client_id_file_path  = string
+      oidc_token_file_path = string
+    }))
+  })
+}
+
+locals {
+  azure_subscription_id     = "db1051fa-ca70-4fed-a721-637b2621c974"
+  azure_resource_group_name = "flux-tests"
 }
 
 resource "aws_s3_bucket" "object_store" {
@@ -43,4 +77,11 @@ resource "google_container_cluster" "main" {
   name             = "gds-hackathon"
   enable_autopilot = true
   project          = local.gcp_project
+}
+
+resource "azurerm_container_registry" "artifact_registry" {
+  location            = "East US"
+  name                = "gdshackathon"
+  resource_group_name = local.azure_resource_group_name
+  sku                 = "Standard"
 }
