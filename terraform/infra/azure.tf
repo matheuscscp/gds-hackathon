04e23dacdf6bda4ac40d45e9b1f920bde21a12b9
Matheus Pimenta <matheuscscp@gmail.com>
2025-05-30 13:52:18 +0100

    Add Azure Managed Identity for GitHub Actions to push the Kubernetes manifests
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

---
+++ terraform/infra/azure.tf
@@ -0,0 +1,56 @@
+provider "azurerm" {
+  features {}
+  subscription_id      = local.azure_subscription_id
+  use_cli              = false
+  use_oidc             = true
+  client_id_file_path  = var.tfc_azure_dynamic_credentials.default.client_id_file_path
+  oidc_token_file_path = var.tfc_azure_dynamic_credentials.default.oidc_token_file_path
+  tenant_id            = "4bd9a3a0-4393-4c05-ab26-77f6c0bfc693"
+}
+
+locals {
+  azure_subscription_id     = "db1051fa-ca70-4fed-a721-637b2621c974"
+  azure_resource_group_name = "flux-tests"
+}
+
+variable "tfc_azure_dynamic_credentials" {
+  description = "Object containing Azure dynamic credentials configuration"
+  type = object({
+    default = object({
+      client_id_file_path  = string
+      oidc_token_file_path = string
+    })
+    aliases = map(object({
+      client_id_file_path  = string
+      oidc_token_file_path = string
+    }))
+  })
+}
+
+resource "azurerm_container_registry" "artifact_registry" {
+  location            = "East US"
+  name                = "gdshackathon"
+  resource_group_name = local.azure_resource_group_name
+  sku                 = "Standard"
+}
+
+resource "azurerm_user_assigned_identity" "push_kubernetes" {
+  name                = "push-kubernetes"
+  location            = "East US"
+  resource_group_name = local.azure_resource_group_name
+}
+
+resource "azurerm_federated_identity_credential" "push_kubernetes" {
+  name                = "push-kubernetes"
+  resource_group_name = local.azure_resource_group_name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = "https://token.actions.githubusercontent.com"
+  subject             = "repo:matheuscscp/gds-hackathon:ref:refs/heads/main"
+  parent_id           = azurerm_user_assigned_identity.push_kubernetes.id
+}
+
+resource "azurerm_role_assignment" "push_kubernetes" {
+  scope                = azurerm_container_registry.artifact_registry.id
+  role_definition_name = "AcrPush"
+  principal_id         = azurerm_user_assigned_identity.push_kubernetes.principal_id
+}
